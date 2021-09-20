95a1c6f9b3ecd93342e24432e275d1015f65cbf2
Martin Matusiak <numerodix@gmail.com>
2021-09-20 21:16:06 +1000

    can read config space

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,70 @@
 use std::fs;
+use std::path;
 use std::u8;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct PciAddress {
+pub struct Address {
+    // 256 possible buses - fits in 1 byte
     bus: u8,
+
+    // 32 possible devices on a bus - fits in 5 bits
     device: u8,
+
+    // 8 possible functions of a device - fits in 3 bits
     function: u8,
 }
 
-impl PciAddress {
+impl Address {
     pub fn new(bus: u8, device: u8, function: u8) -> Self {
         Self {
             bus,
             device,
             function,
         }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(packed)]
+pub struct ConfigSpace {
+    vendor_id: u16,
+    device_id: u16,
+    command: u16,
+    status: u16,
+    revision: u8,
+    class_code1: u8,
+    class_code2: u16,
+    cache_line: u8,
+    latency_timer: u8,
+    header_type: u8,
+    bist: u8,
+    base_addr_0: u32,
+    base_addr_1: u32,
+    base_addr_2: u32,
+    base_addr_3: u32,
+    base_addr_4: u32,
+    base_addr_5: u32,
+    cardbus_cis_pointer: u32,
+    subsystem_vendor_id: u16,
+    subsystem_device_id: u16,
+    expansion_rom_base_addr: u32,
+    reserved1: u64,
+    irq_line: u8,
+    irq_pin: u8,
+    min_grant: u8,
+    max_latency: u8,
+}
+
+impl ConfigSpace {}
+
+impl From<Vec<u8>> for ConfigSpace {
+    fn from(bytes: Vec<u8>) -> Self {
+        assert!(bytes.len() >= 64);
+
+        let ptr = bytes.as_ptr() as *const ConfigSpace;
+        let reference: &ConfigSpace = unsafe { &*ptr };
+
+        reference.clone()
     }
 }
 
@@ -29,7 +79,7 @@ impl Scanner {
         }
     }
 
-    fn scan(&self) -> Vec<PciAddress> {
+    fn scan(&self) -> Vec<Address> {
         let mut addresses = vec![];
 
         // enumerate pci buses
@@ -49,23 +99,30 @@ impl Scanner {
                 let device_entry = device_entry.unwrap();
 
                 let device_func = device_entry.file_name();
-                let device_path = device_entry.path();
-
                 let parts: Vec<&str> = device_func.to_str().unwrap().split('.').collect();
-                println!("{:?}", device_path);
 
                 let bus_no: u8 = u8::from_str_radix(bus_id, 16).unwrap();
                 let dev_no: u8 = u8::from_str_radix(parts[0], 16).unwrap();
                 let func_no: u8 = u8::from_str_radix(parts[1], 16).unwrap();
 
-                let address = PciAddress::new(bus_no, dev_no, func_no);
+                let address = Address::new(bus_no, dev_no, func_no);
                 addresses.push(address);
             }
         }
 
         addresses.sort();
-
         addresses
+    }
+
+    fn load_space(&self, address: &Address) -> ConfigSpace {
+        let bus_name = format!("{:02x}", address.bus);
+        let dev_func = format!("{:02x}.{:x}", address.device, address.function);
+        let path = path::Path::new(&self.root_dir)
+            .join(bus_name)
+            .join(dev_func);
+
+        let bytes = fs::read(&path).unwrap();
+        ConfigSpace::from(bytes)
     }
 }
 
@@ -75,6 +132,11 @@ fn main() {
     let addresses = scanner.scan();
 
     for address in addresses {
-        println!("{:02x}:{:02x}.{:x}", address.bus, address.device, address.function);
+        print!(
+            "{:02x}:{:02x}.{:x} ",
+            address.bus, address.device, address.function
+        );
+        let conf = scanner.load_space(&address);
+        println!("{:?}", conf);
     }
 }
